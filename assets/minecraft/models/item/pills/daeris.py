8216055fd1480cb986f96fa9b61fb1572e3614a1
Yaroslav <yaroslavlyakhov123456789@gmail.com>
2024-09-18 02:40:40 +0300

    pills readded

---
+++ assets/minecraft/models/item/pills/daeris.py
@@ -0,0 +1,5 @@
+for i in range(60):
+    f=open(f"pill_{i}.json",mode='w')
+    a='{"parent": "minecraft:item/generated","textures": {"layer0": "minecraft:item/pills/pill_'+str(i)+'"}}'
+    f.write(a)
+    f.close()
