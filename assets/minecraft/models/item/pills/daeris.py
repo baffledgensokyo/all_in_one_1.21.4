9cb9ff4a6ecc55e6aad80fd561eba247bf9a6076
Yaroslav <yaroslavlyakhov123456789@gmail.com>
2024-09-21 02:39:00 +0300

    new pills

--- assets/minecraft/models/item/pills/daeris.py
+++ assets/minecraft/models/item/pills/daeris.py
@@ -1,4 +1,4 @@
-for i in range(60):
+for i in range(60,145):
     f=open(f"pill_{i}.json",mode='w')
     a='{"parent": "minecraft:item/generated","textures": {"layer0": "minecraft:item/pills/pill_'+str(i)+'"}}'
     f.write(a)
